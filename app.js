47b6b517efa1dffe84fa9aa3f9a6a48f322de8f7
Santi Peris <perisdev>
2019-12-06 16:29:58 +0100

    creamos estructura principal de proyecto.

---
+++ app.js
@@ -0,0 +1,36 @@
+const db = require('./models');
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+let app = express();
+
+//CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, key");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.options('/*',(req,res)=>res.send())  // response status 200 for all options. to fix options issue
+// ...
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// CONNECTION to mariaDB
+// db.sequelize.sync({ force: false });
+
+module.exports = app;
