6aeed58a2afa2f2b7209a246fffb977e23bc848d
Santi Peris <perisdev>
2019-12-12 14:50:23 +0100

    agrupamos products controllers

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -6,7 +6,7 @@
 
 import db from '../models';
 
-const productsController = (req, res, next) => {
+const allController = (req, res, next) => {
 
   db.sequelize.query(req.select, { type: db.sequelize.QueryTypes.SELECT })
     .then(items => (items[0]) ? res.status(200).json(items) : next())
@@ -14,7 +14,45 @@ const productsController = (req, res, next) => {
     
 };
 
-module.exports = productsController;
+
+/** 
+ * responds with all products in promotion.
+ * 
+ * accepts filter by 'id' or 'name' or 'category'.
+ * -----------------------------------------------*/
+
+const promoController = (req, res, next) => {
+
+  db.sequelize.query(req.select.concat(' && products.promo = 1'), { type: db.sequelize.QueryTypes.SELECT })
+    .then(items => (items[0]) ? res.status(200).json(items) : next())
+    .catch(err => res.status(500).json({ message: `products error: ${err}` }));  
+    
+};
+
+
+/** 
+ * add a new product in DB
+ * -----------------------------------------------*/
+
+const addController = (req, res, next) => {
+
+};
+
+/** 
+ * update a product in DB
+ * -----------------------------------------------*/
+
+const updateController = (req, res, next) => {
+
+};
+
+
+module.exports = { 
+  allController,
+  promoController,
+  addController,
+  updateController
+};
 
 
 // db.Order.findAll({
--- routes/products.js
+++ routes/products.js
@@ -5,15 +5,20 @@ const router = express.Router();
 import productsFilterMiddleware from '../middlewares/productsFilterMiddleware';
 
 // controllers
-import productsController from '../controllers/productsController';
-import promoController from '../controllers/promoController';
+import {
+  allController,
+  promoController,
+  addController,
+  updateController
+} from '../controllers/productsController';
 
 // routes
 router.use(productsFilterMiddleware);
 
-router.get('/', productsController);
+router.get('/', allController);
 router.get('/promo', promoController);
-
+router.get('/add', addController);
+router.get('/update', updateController);
 
 // 404 default
 router.use((req, res) => res.status(404).json({ message: 'The resource you requested could not be found' }))
