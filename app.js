0778339a59c3ea30f5a2efebcd460b5efb4d456b
Santi Peris <perisdev>
2020-01-03 12:03:37 +0100

    agregamos headers authorization option

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ let app = express();
 //CORS
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, key");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, key, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, PATCH , PUT, DELETE, OPTIONS");
   next();
 });
